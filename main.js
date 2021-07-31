ce9baed4192a5e48bf0d5e99c1caf0c751b9b136
IndraProgrammer <74811046+IndraProgrammer@users.noreply.github.com>
2021-07-31 12:55:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function preload(){}
+var nosex=0;
+var nosey=0;
+function preload(){
+    clownnose=loadImage("https://i.postimg.cc/4NvQbGHS/clownnose.png");
+}
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
@@ -7,22 +11,25 @@ function setup(){
     video.hide();
     posenet=ml5.poseNet(video,modelLoaded);
     posenet.on('pose',gotPoses);
+
 }
 function modelLoaded(){
     console.log ("modelLoaded");
 }
 function draw(){
     image(video,0,0,300,300);
-
+    image(clownnose,nosex-13,nosey-5,30,30);
 }
 function take_snapshot(){
     save("Filter.png");
 
 }
 function gotPoses(results){
-    //if(results.lenght>0){
+    if(results.length>0){
         console.log(results);
         console.log("nosex="+results[0].pose.nose.x);
         console.log("nosey="+results[0].pose.nose.y);
-    //}
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+   }
 }
